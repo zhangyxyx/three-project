84f0442d8e2536d5198849f42a280acf9753148d
zhangyxyx <zyx930719@126.com>
2020-10-12 15:49:01 +0800

    1012

--- three geometry/js/index.js
+++ three geometry/js/index.js
@@ -27,19 +27,19 @@ function cutovereventgroupfunc() {
     </div>`)
     function main(result) {
         scene = new THREE.Scene();
-        var geometry = new THREE.CylinderGeometry(1, 20, 20, 3);
+        var geometry = new THREE.CylinderGeometry(1, 100, 100, 3);
         var material = new THREE.MeshBasicMaterial({ color: '#3BDCF6' });//0x3BDCF6
         var cone = new THREE.Mesh(geometry, material);
-        cone.position.set(0, 40, 0)
+        cone.position.set(0, 200, 0)
         scene.add(cone);
         //指标2
-        var geometry1 = new THREE.CylinderGeometry(20, 40, 20, 3);
+        var geometry1 = new THREE.CylinderGeometry(100, 200, 100, 3);
         var material1 = new THREE.MeshBasicMaterial({ color: '#fd947e', side: THREE.DoubleSide });
         var cylinder1 = new THREE.Mesh(geometry1, material1);
-        cylinder1.position.set(0, 20, 0)
+        cylinder1.position.set(0, 100, 0)
         scene.add(cylinder1);
         //指标3
-        var geometry2 = new THREE.CylinderGeometry(40, 60, 20, 3);
+        var geometry2 = new THREE.CylinderGeometry(200, 300, 100, 3);
         var material2 = new THREE.MeshBasicMaterial({ color: '#4A5BE7', side: THREE.DoubleSide });
         var cylinder2 = new THREE.Mesh(geometry2, material2);
         cylinder2.position.set(0, 0, 0)
@@ -51,9 +51,9 @@ function cutovereventgroupfunc() {
         scene.add(point); //点光源添加到场景中
         //文字
         var textdata = [
-            { "text": result[0]['vendorname'] + ':' + result[0]['num'], "num": result[0]['num'], 'pos': [2, 40, 10] },
-            { "text": result[1]['vendorname'] + ':' + result[1]['num'], "num": result[1]['num'], 'pos': [13, 20, 15] },
-            { "text": result[2]['vendorname'] + ':' + result[2]['num'], "num": result[2]['num'], 'pos': [26, 0, 10] }
+            { "text": result[0]['vendorname'] + ':' + result[0]['num'], "num": result[0]['num'], 'pos': [25, 200, 25] },
+            { "text": result[1]['vendorname'] + ':' + result[1]['num'], "num": result[1]['num'], 'pos': [75, 100, 75] },
+            { "text": result[2]['vendorname'] + ':' + result[2]['num'], "num": result[2]['num'], 'pos': [125, 0, 125] }
         ]
         var loader = new THREE.FontLoader();
         loader.load('./js/three/MicrosoftYaHei_Regular.json', function (font) {
@@ -62,7 +62,7 @@ function cutovereventgroupfunc() {
                     color: '#000',
                 });
                 var text = new THREE.Mesh(new THREE.TextGeometry(textdata[i]['text'], {
-                    font: font, size: 4, height: 0.1,
+                    font: font, size: 10, height: 0.1,
 
                 }), materialtext);
                 text.position.set(textdata[i]['pos'][0], textdata[i]['pos'][1], textdata[i]['pos'][2])
@@ -81,9 +81,9 @@ function cutovereventgroupfunc() {
         var k = width / height; //窗口宽高比
         var s = 100; //三维场景显示范围控制系数，系数越大，显示的范围越大
         //创建相机对象
-        camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
-        camera.position.set(0, 40, 50);
-        camera.lookAt(new THREE.Vector3(0, 0, 200));
+        camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 10000);
+        camera.position.set(0, 40, 600);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
 
         renderer = new THREE.WebGLRenderer({ alpha: true });
         renderer.setSize(width, height);//设置渲染区域尺寸
@@ -91,7 +91,7 @@ function cutovereventgroupfunc() {
         $("#cutovereventgroup").append(renderer.domElement)
         //执行渲染操作   指定场景、相机作为参数
         // renderer.render(scene, camera);
-        // var axesHelper = new THREE.AxesHelper(250);
+        // var axesHelper = new THREE.AxesHelper(500);
         // scene.add(axesHelper);
 
     }
